4c5925f59186496892714b35faf17272b512e2f8
Syed Nawaz Ahmed <nsyed.4098@gmail.com>
2020-06-20 20:02:54 +0530

    edited bikeshare code once

--- bikeshare.py
+++ bikeshare.py
@@ -1,9 +1,9 @@
-from datetime import timedelta
+
 import time
 import pandas as pd
 import numpy as np
 import click
-
+from datetime import timedelta
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
